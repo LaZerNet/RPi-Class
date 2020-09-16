d0b339d5effe72cb45f4eaeae4ff26f2e2dc84f1
lazeriot@gmail.com <lazeriot@gmail.com>
2020-09-16 14:53:22 -0400

    patch fixes script

--- fixes.sh
+++ fixes.sh
@@ -4,8 +4,8 @@
 pip install Adafruit_BBIO
 
 # Patch required for DHT11 (Add RPi 4 support to adafruit library)
-patch << EOF
---- platform_detect.py	2020-09-16 13:23:46.609992081 -0400
+sudo patch -d/ -p0 << EOF
+--- /usr/local/lib/python3.7/dist-packages/Adafruit_DHT-1.4.0-py3.7-linux-armv7l.egg/Adafruit_DHT/platform_detect.py	2020-09-16 13:24:17.939870559 -0400
 +++ /usr/local/lib/python3.7/dist-packages/Adafruit_DHT-1.4.0-py3.7-linux-armv7l.egg/Adafruit_DHT/platform_detect.py	2020-09-16 13:24:17.939870559 -0400
 @@ -108,6 +108,9 @@
      elif match.group(1) == 'BCM2837':
@@ -20,8 +20,8 @@ patch << EOF
 EOF
 
 # Patch required for LED_Matrix
-patch << EOF
---- serial.py	2020-09-16 13:27:54.358925663 -0400
+sudo patch -d/ -p0 << EOF
+--- /usr/local/lib/python2.7/dist-packages/luma.core-1.12.0-py2.7.egg/luma/core/interface/serial.py	2020-09-16 13:27:16.119104294 -0400
 +++ /usr/local/lib/python2.7/dist-packages/luma.core-1.12.0-py2.7.egg/luma/core/interface/serial.py	2020-09-16 13:27:16.119104294 -0400
 @@ -278,7 +278,7 @@
          try:
